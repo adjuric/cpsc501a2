d91e7a0b0022bfe01d71e6ad109d09c5987bceb1
Aleks <Aleks@Aleks-PC>
2016-10-27 17:18:04 -0600

    Revising Test File - Adding Structure

---
+++ cpsc501a2/src/InspectorTest.java
@@ -0,0 +1,35 @@
+import static org.junit.Assert.*;
+import java.lang.reflect.Field;
+import org.junit.Test;
+
+public class InspectorTest {
+
+	@Test
+	public void testA() {
+		
+	}
+	
+	@Test
+	public void testB() {
+		
+	}
+	
+	@Test
+	public void testConstructorA(){
+		
+	}
+	@Test
+	public void testConstructorB(){
+	
+	}
+	@Test
+	public void testFindFieldA(){
+	}
+	
+	@Test
+	public void testFindFieldB(){
+		
+	}
+	
+
+}
